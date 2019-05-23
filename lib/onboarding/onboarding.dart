c725bebef3e79e374a98203c077e4988ee1fd706
Alaric Hartsock <alaric.hartsock@yahoo.com>
2019-05-22 22:03:33 -0700

    small commit
    added theme boilerplates, added BLoC to pubspec. slightly started working on onboarding. added a directory for our logic side of the app, and added themebloc.dart.
    
    todo: finish out everything that I just added.

--- lib/onboarding/onboarding.dart
+++ lib/onboarding/onboarding.dart
@@ -6,17 +6,36 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> { //todo: stylize colors, stylize to look like onboarding page.
+
   @override
   Widget build(BuildContext context) {
+
+    double height = MediaQuery.of(context).size.width; //fetching width and height values of device
+    double width = MediaQuery.of(context).size.height;
+
     return Scaffold( 
       body: Container(
+        color: Color(0xFFE6EAEF), //tood: pair color to flutter BLoC theme provider
         width: double.infinity,
         height: double.infinity,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
           children: <Widget>[
-
+            Container(),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text("Trade smart"),
+                  Text("Filter stocks by metrics like short interest and short interest change"),
+                  Row(
+                    children: <Widget>[
+                      Container(),
+                      Container(),
+                      Text("NEXT")
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/theme/darkTheme.dart
+++ lib/theme/darkTheme.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = new ThemeData(
-  
-
+  backgroundColor: Color(0xFFE6EAEF),
+  canvasColor: Color(0xFFFFFFFF),
+  primaryColor: Color(0xFF21CE99),
+  accentColor: Color(0xFFF45531), 
+  textTheme: TextTheme(
+    display1: TextStyle(),
+    display2: TextStyle(),
+    display3: TextStyle(),
+    display4: TextStyle(),
+    headline: TextStyle(),
+    title: TextStyle(),
+    subhead: TextStyle(),
+    body1: TextStyle(),
+    body2: TextStyle(),
+    caption: TextStyle(),
+    button: TextStyle(),
+    subtitle: TextStyle(),
+    overline: TextStyle(),
+  ),
 ); 
