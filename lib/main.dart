d95e0db467d026affe717fe239fd999aa26b5452
Kira Corbett <kirac2093@gmail.com>
2019-06-04 09:42:09 -0700

    ios main.dart update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import './logic/themebloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'android/dash/landing.dart';
 
-void main() => runApp(SmartMoneyAndroid()); 
+bool runAndroid = true;
+
+void main() => runApp((runAndroid == true) ? SmartMoneyAndroid() : SmartMoneyIOS());
+
+class SmartMoneyIOS extends StatefulWidget {
+  @override
+  _SmartMoneyIOSState createState() => _SmartMoneyIOSState();
+}
+
+class _SmartMoneyIOSState extends State<SmartMoneyIOS> {
+
+  final ThemeBloc themeBloc = ThemeBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: themeBloc,
+      child: BlocBuilder(
+        bloc: themeBloc,
+        builder: (context,ThemeData theme) {
+          return CupertinoApp( 
+          title: "Smartmoney",
+          home: Landing() //todo: change to onboarding
+          );
+        },
+      )
+    );
+  }
+}
 
 class SmartMoneyAndroid extends StatelessWidget {
 
